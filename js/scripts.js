13e993739e60bfa643d907fe1c189e11b5bb52a5
postolenko <denis.postolenko@gmail.com>
2021-01-29 04:29:20 +0200

    Table

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-function isInViewport(node) {
-  var rect = node.getBoundingClientRect()
-  return (
-    (rect.height > 0 || rect.width > 0) &&
-    rect.bottom >= 0 &&
-    rect.right >= 0 &&
-    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-  )
-}
+// function isInViewport(node) {
+//   var rect = node.getBoundingClientRect()
+//   return (
+//     (rect.height > 0 || rect.width > 0) &&
+//     rect.bottom >= 0 &&
+//     rect.right >= 0 &&
+//     rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+//   )
+// }
 
 function getAnimation() {
   $(".animate").each(function() {
@@ -27,6 +27,14 @@ function getHeaderParams() {
     }
 }
 
+function getThumbsParams() {
+  $(".categorie_thumb").each(function() {
+    $(this).css({
+      "height" : $(this).width() + "px"
+    });
+  });
+}
+
 var w = window,
 d = document,
 e = d.documentElement,
@@ -35,29 +43,26 @@ bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
 
 $(window).load(function() {
-
-
-
 });
 
 $(window).resize(function() {
   bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
   getAnimation();
   getHeaderParams();
+  getThumbsParams();
 
 });
 
 $(document).scroll(function() {
-
   getAnimation();
   getHeaderParams();
-
 });
 
 $(document).ready(function() {
   bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
   getAnimation();
   getHeaderParams();
+  getThumbsParams();
 
     if( $(".object_slider").length > 0 ) {
         $(".object_slider").not(".slick-initialized").slick({
@@ -99,40 +104,40 @@ $(document).ready(function() {
 
 
   // Populate images from data attributes.
-  var scrolled = $(window).scrollTop()
-  $('.parallax').each(function(index) {
-      var imageSrc = $(this).data('image-src')
-      var imageHeight = $(this).data('height')
-      $(this).css('background-image','url(' + imageSrc + ')')
-      $(this).css('height', imageHeight)
+  // var scrolled = $(window).scrollTop()
+  // $('.parallax').each(function(index) {
+  //     var imageSrc = $(this).data('image-src')
+  //     var imageHeight = $(this).data('height')
+  //     $(this).css('background-image','url(' + imageSrc + ')')
+  //     $(this).css('height', imageHeight)
 
-      // Adjust the background position.
-      var initY = $(this).offset().top
-      var height = $(this).height()
-      var diff = scrolled - initY
-      var ratio = Math.round((diff / height) * 100)
-      $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
-  })
+  //     // Adjust the background position.
+  //     var initY = $(this).offset().top
+  //     var height = $(this).height()
+  //     var diff = scrolled - initY
+  //     var ratio = Math.round((diff / height) * 100)
+  //     $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
+  // })
 
-  // Attach scroll event to window. Calculate the scroll ratio of each element
-  // and change the image position with that ratio.
-  // https://codepen.io/lemagus/pen/RWxEYz
-  $(window).scroll(function() {
-    var scrolled = $(window).scrollTop()
-    $('.parallax').each(function(index, element) {
-      var initY = $(this).offset().top
-      var height = $(this).height()
-      var endY  = initY + $(this).height()
+  // // Attach scroll event to window. Calculate the scroll ratio of each element
+  // // and change the image position with that ratio.
+  // // https://codepen.io/lemagus/pen/RWxEYz
+  // $(window).scroll(function() {
+  //   var scrolled = $(window).scrollTop()
+  //   $('.parallax').each(function(index, element) {
+  //     var initY = $(this).offset().top
+  //     var height = $(this).height()
+  //     var endY  = initY + $(this).height()
 
-      // Check if the element is in the viewport.
-      var visible = isInViewport(this)
-      if(visible) {
-        var diff = scrolled - initY
-        var ratio = Math.round((diff / height) * 100)
-        $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
-      }
-    })
-  });
+  //     // Check if the element is in the viewport.
+  //     var visible = isInViewport(this)
+  //     if(visible) {
+  //       var diff = scrolled - initY
+  //       var ratio = Math.round((diff / height) * 100)
+  //       $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
+  //     }
+  //   })
+  // });
 
   // ------------
 
@@ -155,5 +160,37 @@ $(document).ready(function() {
               $(".respmenubtn").removeClass("active");
       }
   });
+
+  // ------------
+
+  new universalParallax().init({
+    speed: 16.0
+});
+
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+
+      // console.log("Вы используете мобильное устройство (телефон или планшет).")
+
+      // $(".show_link_wrapp").mouseenter(function() {
+      //   $(this).addClass("over");
+      // });
+
+      // $(".show_link_wrapp").mousealive(function() {
+      //   $(this).removeClass("over");
+      // });
+
+      // $(".show_link_wrapp").mousedown()(function(e) {
+      //   e.preventDefault();
+      //   $(this).addClass("over");
+      // });
+
+      $( ".show_link_wrapp" ).mousedown(function(e) {
+        e.preventDefault();
+        $(this).addClass("over");
+      });
+
+  } else {
+      // console.log("Вы используете ПК.")
+  }
 
 });
